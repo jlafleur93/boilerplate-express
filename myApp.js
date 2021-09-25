d6a0863c50681a9ee6e9b877759541290855f607
joel-wowz <josephlafleur@hotmail.com>
2021-09-25 16:31:47 -0600

    finished first part of fcc

--- myApp.js
+++ myApp.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-
-
+app.get("/", function(req, res){
+  
+  const filePath =__dirname + "/views/index.html"
+  res.sendFile(filePath)
+})
 
 
 
